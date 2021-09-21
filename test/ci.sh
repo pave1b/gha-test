6d0841953c07770b803ed2dd7722ffb26e921afd
fintros <extmail@alsp.net>
2021-09-21 19:20:25 +0300

    Update ci.sh
    Update CI script

--- test/ci.sh
+++ test/ci.sh
@@ -5,25 +5,8 @@ REST=https://cloud.all-hw.com/ci
 # Verbose output on REST API requests/responses: either "-v" or empty
 VERBOSE=
 
-P_2=$2
-P_3=$3
+TOKEN=`curl -X POST -F firmware=$INPUT_BINARY -F input=$INPUT_FILE $VERBOSE "$REST/usertask?timeout=$INPUT_TIMEOUT&key=$INPUT_API_KEY" 2>/dev/null`
 
-task_status() {
-    curl -X GET $VERBOSE "$REST/usertask?id=$P_2"
-	echo
-}
-
-create_task() {
-    curl -X POST -F firmware=$INPUT_BINARY -F input=$INPUT_FILE $VERBOSE "$REST/usertask?timeout=$INPUT_TIMEOUT&key=$INPUT_API_KEY"
-}
-
-case "$1" in
-    status)
-        task_status
-        ;;
-    task)
-        create_task
-        ;;
-esac
+curl -X GET $VERBOSE "$REST/usertask?id=$TOKEN 2>/dev/null"
 
 exit 0
