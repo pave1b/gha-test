9c2c1dcc5f2e5c0280e7fc11afa36fa641fea7b4
fintros <extmail@alsp.net>
2021-09-21 19:56:19 +0300

    Update ci.sh

--- test/ci.sh
+++ test/ci.sh
@@ -5,7 +5,7 @@ REST=https://cloud.all-hw.com/ci
 # Verbose output on REST API requests/responses: either "-v" or empty
 VERBOSE=
 
-echo curl -X POST -F firmware=.$INPUT_BINARY -F input=.$INPUT_FILE $VERBOSE "$REST/usertask?timeout=$INPUT_TIMEOUT&key=$INPUT_API_KEY
+echo "curl -X POST -F firmware=.$INPUT_BINARY -F input=.$INPUT_FILE $VERBOSE \"$REST/usertask?timeout=$INPUT_TIMEOUT&key=$INPUT_API_KEY\""
 
 TOC=`curl -X POST -F firmware=.$INPUT_BINARY -F input=.$INPUT_FILE $VERBOSE "$REST/usertask?timeout=$INPUT_TIMEOUT&key=$INPUT_API_KEY" 2>/dev/null`
 
